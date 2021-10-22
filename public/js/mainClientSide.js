096c1dc6b1f93f463e44cc6b909eeaa66c4a78bd
GNataliya <80199130+GNataliya@users.noreply.github.com>
2021-10-23 00:29:18 +0300

    Update mainClientSide.js

--- public/js/mainClientSide.js
+++ public/js/mainClientSide.js
@@ -2,22 +2,18 @@ const productsCell = document.querySelector('.products');
 const linkSignIn = document.querySelector('.linkSignIn');
 
 const checkUserAccess = async () => {
+    
     const accessToken = await localStorage.getItem('userAccessKey');
-    // const token = JSON.parse(accessToken);
-    console.log('get from localStore', accessToken)
+    
     const { data } = await axios.post('/authorisation/checkUserToken', accessToken );
-    console.log('19 - checkUser', data);
+    
     if(data.status === 'unauthorisate'){
         window.location.href = '/authorisation'
-        // registrEl.classList.remove('hidden');  
-        // authEl.classList.add('.hidden');
     } else if (data.status === 'ok'){
-        // linkSignIn.classList.add('hidden');
-        // window.location.href = '/'
         linkSignIn.innerHTML = `<h3>${data.payload.name}</h3>`;
-        // console.log('after status', data)
-    }
-}
+    };
+    
+};
 checkUserAccess();
 
 
